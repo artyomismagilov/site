a4a4a50ceb52166af90b77edffa8e63417a14e59
artyomismagilov <saturnn54@gmail.com>
2026-04-16 21:05:12 +0500

    Добавлена библиотека Glide и разработан слайдер

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 window.addEventListener('scroll', e => {
-    document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
+    document.documentElement.style.setProperty('--scrollTop', `${window.scrollY}px`)
 })
 
 window.addEventListener('scroll', e => {
-    document.documentElement.style.setProperty('--scrollTop2', `${this.scrollY}`)
+    document.documentElement.style.setProperty('--scrollTop2', `${window.scrollY}`)
 })
 
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
     wrapper: '.wrapper',
     content: '.content'
