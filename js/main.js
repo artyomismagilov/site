be58214e609bb51d691abd5507c2687ef13acabc
artyomismagilov <saturnn54@gmail.com>
2026-04-21 20:03:37 +0500

    Доработки с блоками + новые анимации

--- js/main.js
+++ js/main.js
@@ -5,6 +5,51 @@ window.addEventListener('scroll', e => {
 window.addEventListener('scroll', e => {
     document.documentElement.style.setProperty('--scrollTop2', `${window.scrollY}`)
 })
+let stop1 = false
+let stop2 = false
+var delay = 0.1;
+const firstBlock = document.querySelectorAll("#firstBlock");
+const anotherBlocks = document.querySelectorAll("#anotherBlock");
+
+window.addEventListener('scroll', e => {
+    const scrollPosition = document.documentElement.scrollTop;
+    // console.log(scrollPosition);
+    if (scrollPosition >= 700 && scrollPosition <= 850 && stop1 == false) {
+        console.log('ОК1')
+        animateBlock(firstBlock)
+        stop1 = true
+    } else if (scrollPosition >= 1500 && scrollPosition <= 1600 && stop2 == false) {
+        console.log('ОК2')
+        animateBlock(anotherBlocks)
+        stop2 = true
+    } else if (scrollPosition <= 200) {
+        clearAnimation()
+        stop1 = false
+        stop2 = false
+    }
+
+
+})
+
+function animateBlock(block) {
+    block.forEach(el => {
+        el.style.animation = "blockLoaded 1.5s ease forwards";
+        el.style.animationDelay = `${delay}s`;
+        delay += 0.1;
+    })
+}
+
+function clearAnimation() {
+    firstBlock.forEach(el => {
+        el.style.animation = "";
+        el.style.animationDelay = ``;
+    })
+    anotherBlocks.forEach(el => {
+        el.style.animation = "";
+        el.style.animationDelay = ``;
+    })
+    delay = 0;
+}
 
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
@@ -46,15 +91,15 @@ const modal6 = document.getElementById('modal6');
 const modal7 = document.getElementById('modal7');
 const modal8 = document.getElementById('modal8');
 const modal9 = document.getElementById('modal9');
-const close1= document.querySelector('.close1');
-const close2= document.querySelector('.close2');
-const close3= document.querySelector('.close3');
-const close4= document.querySelector('.close4');
-const close5= document.querySelector('.close5');
-const close6= document.querySelector('.close6');
-const close7= document.querySelector('.close7');
-const close8= document.querySelector('.close8');
-const close9= document.querySelector('.close9');
+const close1 = document.querySelector('.close1');
+const close2 = document.querySelector('.close2');
+const close3 = document.querySelector('.close3');
+const close4 = document.querySelector('.close4');
+const close5 = document.querySelector('.close5');
+const close6 = document.querySelector('.close6');
+const close7 = document.querySelector('.close7');
+const close8 = document.querySelector('.close8');
+const close9 = document.querySelector('.close9');
 const glide = document.getElementById('glide');
 
 
@@ -66,14 +111,20 @@ function openModal(modalId) {
     if (glideElement) glideElement.style.display = 'none';
     const modal = document.getElementById(modalId);
     if (modal) {
-        modal.style.display = 'flex';
+        modal.style.animation = "fadeIn 0.3s ease"
+        setTimeout(() => {
+            modal.style.display = 'flex';
+        }, 300);
     }
 }
 
 function closeModal(modalId) {
     const modal = document.getElementById(modalId);
     if (modal) {
-        modal.style.display = 'none';
+        modal.style.animation = "fadeOut 0.3s ease"
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 300);
     }
     if (glideElement) glideElement.style.display = 'flex';
 }
@@ -84,7 +135,7 @@ if (glideElement) {
     glideElement.addEventListener('click', (e) => {
         // Ищем ближайший родительский элемент слайда с ID типа openWord1
         const slide = e.target.closest('.glide__slide');
-        
+
         if (slide && slide.id) {
             // Извлекаем номер из ID (например, из "openWord8" получим "8")
             const number = slide.id.replace('openWord', '');
@@ -111,67 +162,3 @@ document.addEventListener('click', (e) => {
         closeModal(e.target.id);
     }
 });
-
-// function openModal(modalId, glideElement) {
-//     return function() {
-//         glideElement.style.display = 'none';
-//         const modal = document.getElementById(modalId);
-//         if (modal) {
-//             modal.style.display = 'flex';
-//         } else {
-//             console.error('Модальное окно не найдено:', modalId);
-//             glideElement.style.display = 'flex'; // Возвращаем обратно, если окна нет
-//         }
-//     }
-// }
-
-
-// function closeModal(modalId, glideElement) {
-//     return function() {
-//         const modal = document.getElementById(modalId);
-//         if (modal) {
-//             modal.style.display = 'none';
-//         }
-//         glideElement.style.display = 'flex';
-//     }
-// }
-
-// const planets = [
-//     { open: open1, modalId: 'modal1', closeClass: '.close1' },
-//     { open: open2, modalId: 'modal2', closeClass: '.close2' },
-//     { open: open3, modalId: 'modal3', closeClass: '.close3' },
-//     { open: open4, modalId: 'modal4', closeClass: '.close4' },
-//     { open: open5, modalId: 'modal5', closeClass: '.close5' },
-//     { open: open6, modalId: 'modal6', closeClass: '.close6' },
-//     { open: open7, modalId: 'modal7', closeClass: '.close7' },
-//     { open: open8, modalId: 'modal8', closeClass: '.close8' },
-//     { open: open9, modalId: 'modal9', closeClass: '.close9' }
-// ];
-
-// planets.forEach(planet => {
-//     if (planet.open) {
-//         planet.open.addEventListener('click', openModal(planet.modalId, glide));
-        
-//         const closeBtn = document.querySelector(planet.closeClass);
-//         if (closeBtn) {
-//             closeBtn.addEventListener('click', closeModal(planet.modalId, glide));
-//         } else {
-//             console.error('Кнопка закрытия не найдена:', planet.closeClass);
-//         }
-//     } else {
-//         console.error('Элемент открытия не найден для:', planet.modalId);
-//     }
-// });
-
-// document.querySelectorAll('.modal').forEach(modal => {
-//     modal.addEventListener('click', (e) => {
-//         if (e.target === modal) {
-//             const modalId = modal.id;
-//             const planetIndex = planets.findIndex(p => p.modalId === modalId);
-//             if (planetIndex !== -1 && planets[planetIndex].open) {
-//                 glide.style.display = 'flex';
-//                 modal.style.display = 'none';
-//             }
-//         }
-//     });
-// });
